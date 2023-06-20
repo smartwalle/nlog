7d22d9fb20eb4e123892c5388772545bfbe05eaf
smartwalle <smartwalle@gmail.com>
2023-06-20 09:35:09 +0800

    test

--- file_test.go
+++ file_test.go
@@ -8,9 +8,28 @@ import (
 	"testing"
 )
 
-func BenchmarkLogger_Write(b *testing.B) {
+func BenchmarkFile_Write(b *testing.B) {
 	var n = rand.Int()
 	var file, err = nlog.NewFile(fmt.Sprintf("logs/%d.log", n))
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.SetParallelism(100)
+	log.SetOutput(file)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
+
+	for i := 0; i < b.N; i++ {
+		log.Println(n, i, "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+	}
+
+	file.Close()
+	b.Log(n, b.N)
+}
+
+func BenchmarkBufferedFile_Write(b *testing.B) {
+	var n = rand.Int()
+	var file, err = nlog.NewFile(fmt.Sprintf("logs/%d.log", n), nlog.WithBuffer(1*1024*1024))
 	if err != nil {
 		b.Fatal(err)
 	}
